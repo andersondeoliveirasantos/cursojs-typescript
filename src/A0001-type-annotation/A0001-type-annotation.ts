0af489c20ed0c2e521fa294791ffd2d7523367db
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-10-26 18:40:15 -0300

    cursojs-typescript.007

---
+++ src/A0001-type-annotation/A0001-type-annotation.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+// Tipos básicos ( aqui ocorre inferência de tipos )
+
+let nome: string = 'Anderson';  // Qualquer tipo de strings: ' ', " ", ` `
+
+let idade: number = 30; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
+
+let adulto: boolean = true; // true or false
+
+let simbolo: symbol = Symbol( 'qualquer-symbol'); // symbol
+
+// let big: bigint = 10n; // bigint
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import fucao from './mod';
-
-fucao();
