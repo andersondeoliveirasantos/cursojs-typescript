c3407c746297e6c9e7a55c846f7d5360e11498e5
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-10-29 19:26:52 -0300

    cursojs-typescript.009

--- src/A0001-type-annotation/A0001-type-annotation.ts
+++ src/A0001-type-annotation/A0001-type-annotation.ts
@@ -15,4 +15,23 @@ let simbolo: symbol = Symbol( 'qualquer-symbol'); // symbol
 
 // Arrays
 let arrayDeNumeros: Array<number> = [1, 2, 3]
+let arrayDeNumeros2: number[] = [1, 2, 3]
+
 let arrayDeStrings: Array<string> = ['a', 'b', 'c']
+let arrayDeStrings2: string[] = ['a', 'b', 'c']
+
+
+// Objetos
+let pessoa: {nome: string, idade: number, adulto?: boolean} = {
+  idade: 30,
+  nome: 'Anderson'
+}
+
+
+// Funções
+function soma(x: number, y: number): number {
+  return x + y
+}
+const result  = soma( 2, 2)
+
+const soma2: (x: number, y: number) => number = (x, y) => x + y
