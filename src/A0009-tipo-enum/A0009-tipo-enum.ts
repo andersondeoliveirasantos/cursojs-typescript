a1c3f9c0209c635ae7566882919258a3890c3cc7
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-10-31 20:20:47 -0300

    cursojs-typescript.018

---
+++ src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -0,0 +1,22 @@
+enum Cores {
+  VERMELHO, // 0
+  AZUL, // 1
+  AMARELO, // 2
+  ROXO = 10, // 10
+  LARANJA = 200, // 200
+}
+
+// Podemos criar outro enum e ele vai unir um com o outro.
+enum Cores {
+  DOURADO = 1,
+  PRATA = 2,
+  OURO = 3,
+}
+
+console.log(Cores);
+
+function escolhaCor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaCor(Cores.ROXO);
