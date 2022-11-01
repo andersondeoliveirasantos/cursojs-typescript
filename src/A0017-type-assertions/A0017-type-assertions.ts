6ba55c3d70a5cb08de362a67c6c5a8aa99eb5241
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-01 14:33:53 -0300

    cursojs-typescript.026

--- src/A0017-type-assertions/A0017-type-assertions.ts
+++ src/A0017-type-assertions/A0017-type-assertions.ts
@@ -1,2 +1,28 @@
-const body = document.querySelector('body');
-if (body) body.style.background = 'red';
+/* *********************************************** */
+/* *************  RECOMENDADO  ************* */
+/* ********************************************** */
+
+// Condicional
+const body1 = document.querySelector('body');
+if (body1) body1.style.background = 'red';
+
+// Type assertion
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
+
+// HTMLElement
+const input = document.querySelector('.input') as HTMLAnchorElement;
+input.value = 'Qualquer coisa';
+input.focus();
+
+/* ********************************************** */
+/* **********  NÃO RECOMENDADO  ********** */
+/* ********************************************** */
+
+// Non-null assertion ( ! )
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+// Type assertion
+const body4 = document.querySelector('body') as unknown as number;
+body4.style.background = 'red';
