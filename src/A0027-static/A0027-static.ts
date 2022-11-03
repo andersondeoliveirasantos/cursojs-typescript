a633ae91adf5aa845d6f49262ff612175ca7cb0f
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-11-03 01:21:35 -0300

    cursojs-typescript.038

---
+++ src/A0027-static/A0027-static.ts
@@ -0,0 +1,31 @@
+export class Pessoa {
+  static idadePadrao = 0;
+  static cpfPadrao = '000.000.000-00';
+
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    public idade: number,
+    public cpf: string,
+  ) {}
+
+  metodoNormal(): void {
+    console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
+
+  static criaPessoa(nome: string, sobrenome: string): Pessoa {
+    return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
+  }
+}
+
+const pessoa1 = new Pessoa(
+  'Anderson',
+  'de Oliveira Santos',
+  30,
+  '123.456.798-00',
+);
+const pessoa2 = Pessoa.criaPessoa('Daniela', 'Cristina');
+console.log(pessoa1);
+console.log(pessoa2);
+pessoa1.metodoNormal();
+console.log(Pessoa.idadePadrao, Pessoa.cpfPadrao);
